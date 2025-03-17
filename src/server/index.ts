54317c1971dceaed455d00d9c83acd2bbc867d7b
Miles Young <miles.j.young@gmail.com>
2025-03-17 13:37:15 -0700

    more basic server setup: logging, middleware, nodemon

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,11 +1,19 @@
 import express from "express"
+import httpErrors from "http-errors"
 
+// custom routes
 import rootRoutes from "./roots/root"
+// custom middleware
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
 app.use("/", rootRoutes)
+
+
+app.use((_, __, next) => {
+    next(httpErrors(404))
+})
 
 app.listen(PORT, () => {
     console.log("server listening at port: ", PORT)
---
+++ src/server/middleware/time.ts
@@ -0,0 +1,8 @@
+import { NextFunction, Request, Response } from "express"
+
+const timeMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    console.log("Request made at: ", (new Date()).toISOString())
+    next()
+}
+
+export {timeMiddleware}
