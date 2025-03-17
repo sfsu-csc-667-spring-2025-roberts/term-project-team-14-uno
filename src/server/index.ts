603fcd4669bbf267226fb1f77e71b6e19f6750c0
Miles Young <miles.j.young@gmail.com>
2025-03-17 14:13:37 -0700

    added husky lint-staged and prettier formatting for pre commits

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,27 +1,31 @@
-import * as path from "path"
-import express from "express"
-import httpErrors from "http-errors"
-import morgan from "morgan"
+import * as path from "path";
+import express, { urlencoded } from "express";
+import httpErrors from "http-errors";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
 
 // custom routes
-import rootRoutes from "./roots/root"
+import rootRoutes from "./roots/root";
 // custom middleware
 
-const app = express()
-const PORT = process.env.PORT || 3000
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.use(morgan("dev"))
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
-app.use(express.static(path.join(process.cwd(), "public")))
-app.set("views", path.join(process.cwd(), "src", "server", "templates"))
-app.set("view engine", "ejs")
+app.use(express.static(path.join(process.cwd(), "public")));
+app.set("views", path.join(process.cwd(), "src", "server", "templates"));
+app.set("view engine", "ejs");
 
-app.use("/", rootRoutes)
+app.use("/", rootRoutes);
 
 app.use((_, __, next) => {
-    next(httpErrors(404))
-})
+  next(httpErrors(404));
+});
 
 app.listen(PORT, () => {
-    console.log("server listening at port: ", PORT)
-})
+  console.log("server listening at port: ", PORT);
+});
